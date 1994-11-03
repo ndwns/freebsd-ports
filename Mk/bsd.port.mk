0b58776eec1da4b947f68807718a7fd86a94a331
jkh <jkh@freebsd.org>
1994-11-03 19:53:46 +0000

    For submakes, don't do a `${MAKE} ${MAKEFILE} all' or `${MAKE} ${MAKEFILE} install', indirect the target through ALL_TARGET and INSTALL_TARGET now, so they can be easily overridden.

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -142,6 +142,9 @@ PKG_CMD?=		pkg_create
 PKG_ARGS?=		-v -c ${PKGDIR}/COMMENT -d ${PKGDIR}/DESCR -f ${PKGDIR}/PLIST -p ${PREFIX}
 PKG_SUFX?=		.tgz
 
+ALL_TARGET?=		all
+INSTALL_TARGET?=	install
+
 # I guess we're in the master distribution business! :)  As we gain mirror
 # sites for distfiles, add them to this list.
 MASTER_SITES+=	ftp://freebsd.cdrom.com/pub/FreeBSD/FreeBSD-current/ports/distfiles/
@@ -246,9 +249,9 @@ ${INSTALL_COOKIE}:
 	@echo "===>  Installing for ${DISTNAME}"
 	@${MAKE} ${.MAKEFLAGS} pre-install
 .if defined(USE_GMAKE)
-	@(cd ${WRKSRC}; ${GMAKE} PREFIX=${PREFIX} ${MAKE_FLAGS} ${MAKEFILE} install)
+	@(cd ${WRKSRC}; ${GMAKE} PREFIX=${PREFIX} ${MAKE_FLAGS} ${MAKEFILE} ${INSTALL_TARGET})
 .else defined(USE_GMAKE)
-	@(cd ${WRKSRC}; ${MAKE} PREFIX=${PREFIX} ${MAKE_FLAGS} ${MAKEFILE} install)
+	@(cd ${WRKSRC}; ${MAKE} PREFIX=${PREFIX} ${MAKE_FLAGS} ${MAKEFILE} ${INSTALL_TARGET})
 .if defined(USE_IMAKE)
 	@(cd ${WRKSRC}; ${MAKE} ${MAKE_FLAGS} ${MAKEFILE} install.man)
 .endif
@@ -297,9 +300,9 @@ ${BUILD_COOKIE}:
 .endif
 .endif
 .if defined(USE_GMAKE)
-	@(cd ${WRKSRC}; ${GMAKE} ${MAKE_FLAGS} ${MAKEFILE} all)
+	@(cd ${WRKSRC}; ${GMAKE} ${MAKE_FLAGS} ${MAKEFILE} ${ALL_TARGET})
 .else defined(USE_GMAKE)
-	@(cd ${WRKSRC}; ${MAKE} ${MAKE_FLAGS} ${MAKEFILE} all)
+	@(cd ${WRKSRC}; ${MAKE} ${MAKE_FLAGS} ${MAKEFILE} ${ALL_TARGET})
 .endif
 	@if [ -f ${SCRIPTDIR}/post-build ]; then \
 		env CURDIR=${.CURDIR} DISTDIR=${DISTDIR} WRKDIR=${WRKDIR} \
