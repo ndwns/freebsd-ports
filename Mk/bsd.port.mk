42c3a7cb82ef1d0656d4ef5aa172e3caa60d8441
jmz <jmz@freebsd.org>
1994-11-16 23:14:22 +0000

    If USE_GMAKE is defined, use ${GMAKE} in ${XMKMF} && ${MAKE} Makefiles

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -368,7 +368,11 @@ ${CONFIGURE_COOKIE}:
 	@(cd ${WRKSRC}; ./configure ${CONFIGURE_ARGS})
 .endif
 .if defined(USE_IMAKE)
+.if defined(USE_GMAKE)
+	@(cd ${WRKSRC}; ${XMKMF} && ${GMAKE} Makefiles)
+.else
 	@(cd ${WRKSRC}; ${XMKMF} && ${MAKE} Makefiles)
+.endif
 .endif
 	@if [ -f ${SCRIPTDIR}/post-configure ]; then \
 		env CURDIR=${.CURDIR} DISTDIR=${DISTDIR} WRKDIR=${WRKDIR} \
