e6be9174acaf51e2b0198f93bd486349567c052f
jkh <jkh@freebsd.org>
1995-01-04 07:35:46 +0000

    Document describe target.

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -89,6 +89,8 @@
 # reinstall		- Install the results of a build, ignoring "already installed"
 #				  flag.
 # package		- Create a package from a build.
+# describe		- Try to generate a one-line description for each port for
+#				  use in INDEX files and the like.
 #
 # Default sequence for "all" is:  fetch extract configure build
 
