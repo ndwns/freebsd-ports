17e18906f4b6fe2ecf110a5207d4cdfb004ab1bb
hsu <hsu@freebsd.org>
1995-02-06 08:52:19 +0000

    Allow arbitrarily named configure script for use w/ HAS_CONFIGURE.

--- Mk/bsd.port.mk
+++ Mk/bsd.port.mk
@@ -70,6 +70,7 @@
 #						target.
 # HAS_CONFIGURE	- Says that the port has its own configure script.
 # GNU_CONFIGURE	- Set if you are using GNU configure (optional).
+# CONFIGURE_SCRIPT - Name of configure script, defaults to 'configure'.
 # CONFIGURE_ARGS - Pass these args to configure, if ${HAS_CONFIGURE} set.
 # IS_INTERACTIVE - Set this if your port needs to interact with the user
 #				  during a build.  User can then decide to skip this port by
@@ -222,6 +223,8 @@ PKGFILE?=		${PACKAGES}/${PKGNAME}${PKG_SUFX}
 .else
 PKGFILE?=		${PKGNAME}${PKG_SUFX}
 .endif
+
+CONFIGURE_SCRIPT?=	configure
 
 .if defined(GNU_CONFIGURE)
 CONFIGURE_ARGS?=	--prefix=${PREFIX}
@@ -534,7 +537,7 @@ ${CONFIGURE_COOKIE}:
 	@(cd ${WRKSRC}; CC="${CC}" ac_cv_path_CC="${CC}" CFLAGS="${CFLAGS}" \
 	    INSTALL="/usr/bin/install -c -o ${BINOWN} -g ${BINGRP}" \
 	    INSTALL_PROGRAM="/usr/bin/install ${COPY} ${STRIP} -o ${BINOWN} -g ${BINGRP} -m ${BINMODE}" \
-	    ./configure ${CONFIGURE_ARGS})
+	    ./${CONFIGURE_SCRIPT} ${CONFIGURE_ARGS})
 .endif
 .if defined(USE_IMAKE)
 .if defined(USE_GMAKE)
