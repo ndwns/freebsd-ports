79438d3049ecf957355f85784b72c1f9f0247d59
fenner <fenner@freebsd.org>
1997-11-06 02:20:27 +0000

    Convert <, > and & into &lt;, &gt; and &amp; in port names, COMMENT and  DESCR files when building README.html . Don't use control characters in sed statement.
    Problems reported by "Chris G. Demetriou" <cgd@pa.dec.com> in NetBSD PR
    pkg/4341.

--- Mk/bsd.port.subdir.mk
+++ Mk/bsd.port.subdir.mk
@@ -119,27 +119,34 @@ README=	${TEMPLATES}/README.top
 README=	${TEMPLATES}/README.category
 .endif
 
+HTMLIFY=	sed -e 's/&/\&amp;/g' -e 's/>/\&gt;/g' -e 's/</\&lt;/g'
+
 README.html:
 	@echo "===>  Creating README.html"
 	@> $@.tmp
 .for entry in ${SUBDIR}
 .if defined(PORTSTOP)
-	@echo -n '<a href="'${entry}/README.html'">${entry}</a>: ' >> $@.tmp
+	@echo -n '<a href="'${entry}/README.html'">'"`echo ${entry} | ${HTMLIFY}`"'</a>: ' >> $@.tmp
 .else
-	@echo -n '<a href="'${entry}/README.html'">'"`cd ${entry}; make package-name`</a>: " >> $@.tmp
+	@echo -n '<a href="'${entry}/README.html'">'"`cd ${entry}; make package-name | ${HTMLIFY}`</a>: " >> $@.tmp
 .endif
 .if exists(${entry}/pkg/COMMENT)
-	@cat ${entry}/pkg/COMMENT >> $@.tmp
+	@${HTMLIFY} ${entry}/pkg/COMMENT >> $@.tmp
 .else
 	@echo "(no description)" >> $@.tmp
 .endif
 .endfor
 	@sort -t '>' +1 -2 $@.tmp > $@.tmp2
+.if exists(${.CURDIR}/pkg/DESCR)
+	@${HTMLIFY} ${.CURDIR}/pkg/DESCR > $@.tmp3
+.else
+	@> $@.tmp3
+.endif
 	@cat ${README} | \
-		sed -e 's%%CATEGORY%%'`echo ${.CURDIR} | sed -e 's.*/\([^/]*\)$$\1'`'g' \
-			-e '/%%DESCR%%/r${.CURDIR}/pkg/DESCR' \
+		sed -e 's/%%CATEGORY%%/'"`basename ${.CURDIR}`"'/g' \
+			-e '/%%DESCR%%/r$@.tmp3' \
 			-e '/%%DESCR%%/d' \
 			-e '/%%SUBDIR%%/r$@.tmp2' \
 			-e '/%%SUBDIR%%/d' \
 		> $@
-	@rm -f $@.tmp $@.tmp2
+	@rm -f $@.tmp $@.tmp2 $@.tmp3
