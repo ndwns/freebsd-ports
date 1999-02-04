29ec03575e187b4394c41e1fdee6ec515360db4d
gj <gj@freebsd.org>
1999-02-04 05:10:39 +0000

    Update to version 3.0. This version only works for FreeBSD 3.0R or higher. Submitted from: Lars Koeller (the maintainer)

---
+++ sysutils/xperfmon3/files/fbsd_vers.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 1998  Lars Köller
+ * All rights reserved.
+ *
+ * Handel the FreeBSD Versions in a central file!
+ *
+ *     $Id$
+ */
+
+#ifdef HAVE_SYS_PARAM_H
+#  include <sys/param.h>
+#endif
+
+#if (defined(BSD) && (BSD >= 199506))
+#  include <osreldate.h>
+#else
+#  error You can compile this xperfmon++ V3.0 only on FreeBSD-3.X systems. xperfmon++ V1.31 up to V2.0 runs on FreeBSD 2.[12].X system!
+#endif
+
+#if __FreeBSD_version < 300000
+#  error You can compile this xperfmon++ V3.0 only on FreeBSD-3.X systems. xperfmon++ V1.31 up to V2.0 runs on FreeBSD 2.[12].X system!
+#endif
