9e7c6290c11d6111c3772490eea4008618aa9241
obrien <obrien@freebsd.org>
1999-10-10 20:28:31 +0000

    Upgrade to the 1999-09-20 development snapshot.

--- lang/gcc-ooo/files/alpha-freebsd.h
+++ lang/gcc-ooo/files/alpha-freebsd.h
@@ -22,6 +22,8 @@ the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.  */
    This was taken from the NetBSD configuration, and modified
    for FreeBSD/alpha by Hidetoshi Shimokawa <simokawa@FreeBSD.ORG> */
 
+/* $FreeBSD$ */
+
 
 /* Get generic FreeBSD definitions.  */
 #include <freebsd.h>
