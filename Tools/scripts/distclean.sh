d19b57e13772e0ece2a9ddc53a27355e25cac68c
sobomax <sobomax@freebsd.org>
2000-06-06 10:05:06 +0000

    Add a traling slash ("/") to the /usr/ports/ and /usr/ports/distfiles/ to make the script working for the cases when either one directory is a symlink.
    Submitted by:	Juriy Goloveshkin <juriy@avias.com>, Volodymyr Kostyrko <arcade@limbo.dn.ua>

--- Tools/scripts/distclean.sh
+++ Tools/scripts/distclean.sh
@@ -31,13 +31,13 @@ FN_DISTFILES=`mktemp -t dclean` || exit 1
 FN_RESULTS_SCRIPT=`mktemp -t dclean` || exit 1
 
 echo -n "Building ports md5 index..."
-find /usr/ports -name "md5" -type f | xargs cat | grep "^MD5 ("| sort | uniq > $FN_PORTS
+find /usr/ports/ -name "md5" -type f | xargs cat | grep "^MD5 ("| sort | uniq > $FN_PORTS
 echo "Done."
 P_MD5_COUNT=`wc -l $FN_PORTS | sed "s| $FN_PORTS|| ; s| ||g"`
 echo "Found $P_MD5_COUNT md5 entries in your ports directory."
 
 echo -n "Building distfiles md5 index..."
-find -H /usr/ports/distfiles -type f | xargs md5 | sed 's|/usr/ports/distfiles/||' | sort > $FN_DISTFILES
+find /usr/ports/distfiles/ -type f | xargs md5 | sed 's|/usr/ports/distfiles/||' | sort > $FN_DISTFILES
 echo "Done."
 D_MD5_COUNT=`wc -l $FN_DISTFILES | sed "s| $FN_DISTFILES|| ; s| ||g"`
 echo "Found $D_MD5_COUNT distfile(s) in your distfiles directory."
