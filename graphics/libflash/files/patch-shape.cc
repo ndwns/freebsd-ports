43a00fec9e1ac212510dabc8db15ce99f625498a
knu <knu@freebsd.org>
2002-07-23 21:07:04 +0000

    Fix build on CURRENT.
    PR:		ports/40606
    Submitted by:	Takanori Saneto <sanewo@ba2.so-net.ne.jp>

---
+++ graphics/libflash/files/patch-shape.cc
@@ -0,0 +1,10 @@
+--- shape.cc.orig	Fri Jul 12 07:55:33 2002
++++ shape.cc	Fri Jul 12 07:56:13 2002
+@@ -21,6 +21,7 @@
+ //
+ 
+ #include "swf.h"
++#include <math.h>
+ 
+ #ifdef RCSID
+ static char *rcsid = "$Id$";
