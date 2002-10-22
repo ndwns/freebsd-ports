63239b6eebb11629141c17ad6324a809966f2861
marcus <marcus@freebsd.org>
2002-10-22 21:51:07 +0000

    Update to 0.9.2.

---
+++ x11-toolkits/vte/files/patch-src_keymap.c
@@ -0,0 +1,10 @@
+--- src/keymap.c.orig	Tue Oct 22 17:42:20 2002
++++ src/keymap.c	Tue Oct 22 17:42:33 2002
+@@ -18,6 +18,7 @@
+ 
+ #ident "$Id$"
+ #include "../config.h"
++#include <sys/types.h>
+ #include <stdlib.h>
+ #include <string.h>
+ #include <glib.h>
