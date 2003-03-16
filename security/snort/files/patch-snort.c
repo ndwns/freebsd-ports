2c734e6fd13a87172eb746a6156c681545aa7001
kris <kris@freebsd.org>
2003-03-16 00:31:55 +0000

    Add a patch to fix runtime problems when snort is built with -DWITH_MYSQL
    Submitted by:	Mike Harding <mvh@ix.netcom.com>
    Obtained from:	snort CVS

--- security/snort/files/patch-snort.c
+++ security/snort/files/patch-snort.c
@@ -1,11 +1,36 @@
---- src/snort.c.orig	Sun Oct  6 02:14:32 2002
-+++ src/snort.c	Sun Oct  6 02:14:18 2002
-@@ -2193,7 +2193,7 @@
+--- src/snort.c.orig	Fri Mar 14 19:58:33 2003
++++ src/snort.c	Fri Mar 14 19:58:19 2003
+@@ -1,4 +1,4 @@
+-/* $Id$ */
++/* $Id$ */
+ /*
+ ** Copyright (C) 1998-2002 Martin Roesch <roesch@sourcefire.com>
+ **
+@@ -252,6 +252,8 @@
+         OpenPcap(pv.readfile, 0);
+     }
+ 
++    InitOutputPlugins();
++    
+     /* didn't get any conf data at the command line, try to find the default
+      * conf file
+      */ 
+@@ -310,9 +312,6 @@
+         GoDaemon();
+     }
+ 
+-    /* initialize these here so we do not print unwanted messages */
+-    InitOutputPlugins();
+-    
+     /*
+      * creating a PID file before setting its proper
+      * path (in SanityChecks()) is not a good idea
+@@ -2212,7 +2211,7 @@
      struct stat st;
      int found;
      int i;
 -    char *conf_files[]={"/etc/snort.conf", "./snort.conf", NULL};
-+    char *conf_files[]={"%%PREFIX%%/etc/snort.conf", "./snort.conf", NULL};
++    char *conf_files[]={"/usr/local/etc/snort.conf", "./snort.conf", NULL};
      char *fname = NULL;
      char *home_dir;
      char *tmp;
