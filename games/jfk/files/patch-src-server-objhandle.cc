13a49f63c8e54dbcc36394af261adc5c2c652ea9
krion <krion@freebsd.org>
2003-10-21 14:57:32 +0000

    - Fix build on -current, unbreak this port - Use DOCSDIR
    PR:		58114
    Submitted by:	maintainer

---
+++ games/jfk/files/patch-src-server-objhandle.cc
@@ -0,0 +1,10 @@
+--- src/server/objhandle.cc.orig	Tue Oct 21 16:49:11 2003
++++ src/server/objhandle.cc	Tue Oct 21 16:49:32 2003
+@@ -2,6 +2,7 @@
+  * Copyright (c) 2002, Stefan Farfeleder <e0026813@stud3.tuwien.ac.at>
+  * $Id$
+  */
++#include <cassert>
+ #include "object_s.h"
+ #include "objhandle.h"
+ 
