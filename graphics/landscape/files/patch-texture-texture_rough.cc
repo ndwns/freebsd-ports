582061e4c20aa942050a4f5e32518bf5e0069b0d
arved <arved@freebsd.org>
2004-02-19 20:00:57 +0000

    Fix build on CURRENT

---
+++ graphics/landscape/files/patch-texture-texture_rough.cc
@@ -0,0 +1,11 @@
+--- texture/texture_rough.cc.orig	Thu Feb 19 20:48:54 2004
++++ texture/texture_rough.cc	Thu Feb 19 20:49:59 2004
+@@ -8,7 +8,7 @@
+ // $Id$
+ //
+ 
+-
++#include <assert.h>
+ #include "texture_rough.h"
+ 
+ 
