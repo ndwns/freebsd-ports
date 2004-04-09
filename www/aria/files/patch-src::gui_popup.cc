6cf7870cd3b68f1465595ade5e29115e47347cbf
krion <krion@freebsd.org>
2004-04-09 18:20:43 +0000

    - Fix build on big-endian arch
    PR:		ports/65371
    Submitted by:	Ports Fury

---
+++ www/aria/files/patch-src::gui_popup.cc
@@ -0,0 +1,11 @@
+--- src/gui_popup.cc.orig	Wed Apr  3 22:33:52 2002
++++ src/gui_popup.cc	Thu Apr  8 22:59:27 2004
+@@ -19,7 +19,7 @@
+ 
+ // $Id$
+ 
+-#include <iostream.h>
++#include <iostream>
+ #include "aria.h"
+ #include "gui_utils.h"
+ 
