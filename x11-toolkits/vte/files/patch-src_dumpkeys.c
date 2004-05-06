b019bbf2a45c689ee1978c615cddbb12a27f6ed7
bland <bland@freebsd.org>
2004-05-06 02:51:51 +0000

    Update to 0.11.11

--- x11-toolkits/vte/files/patch-src_dumpkeys.c
+++ x11-toolkits/vte/files/patch-src_dumpkeys.c
@@ -1,12 +1,15 @@
---- src/dumpkeys.c.orig	Wed Dec  4 02:04:12 2002
-+++ src/dumpkeys.c	Wed Dec  4 02:04:30 2002
-@@ -18,11 +18,11 @@
+--- src/dumpkeys.c.orig	Fri Apr 30 10:16:53 2004
++++ src/dumpkeys.c	Thu May  6 10:46:19 2004
+@@ -18,6 +18,7 @@
  
  #ident "$Id$"
  #include "../config.h"
 +#include <sys/types.h>
  #ifdef HAVE_SYS_SELECT_H
  #include <sys/select.h>
+ #endif
+@@ -25,7 +26,6 @@
+ #include <sys/termios.h>
  #endif
  #include <sys/time.h>
 -#include <sys/types.h>
