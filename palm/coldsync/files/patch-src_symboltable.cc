80d44658788c19174508c848068d3ab6d6b91559
pav <pav@freebsd.org>
2004-05-11 09:28:50 +0000

    - Fix build on 5.x and unbreak
    PR:		ports/66488
    Submitted by:	Arne Woerner <arne_woerner@yahoo.com>

--- palm/coldsync/files/patch-src_symboltable.cc
+++ palm/coldsync/files/patch-src_symboltable.cc
@@ -1,14 +1,18 @@
-
-$FreeBSD$
-
---- src/symboltable.cc.orig	Fri Feb 21 23:32:21 2003
-+++ src/symboltable.cc	Fri Feb 21 23:35:23 2003
-@@ -13,6 +13,8 @@
+--- src/symboltable.cc.orig	Tue Nov 13 16:48:07 2001
++++ src/symboltable.cc	Tue May 11 11:19:40 2004
+@@ -7,11 +7,15 @@
+  * $Id$
+  */
+ 
++#include <stdio.h>
++#include <wchar.h>
+ #include "config.h"
+ #include <string>
+ #include <map>
  #include <cstdlib>		// For malloc() and friends
  #include "symboltable.h"
- 
-+using namespace std;
 +
++using namespace std;
+ 
  static char *make_c_string(const string &s);
  
- map<string,string> table;	/* XXX - Is this going to cause problems on
