49db79808fe8d18a0e70ca6ae2ecfe37d9b6def3
krion <krion@freebsd.org>
2004-05-25 09:17:51 +0000

    Add minisapserver 0.2.3, SAP and SLP announcement for the VLC Media Player.
    PR:		ports/67162
    Submitted by:	michael johnson <ahze@ahze.net>

---
+++ net/minisapserver/files/sapserver.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# $FreeBSD$
+#
+
+# PROVIDE: sapserver
+# REQUIRE: NETWORKING
+# KEYWORD: FreeBSD shutdown
+
+#
+# Add the following lines to /etc/rc.conf to enable sapserver:
+#
+#sapserver_enable="YES"
+#
+sapserver_enable="NO"
+
+. %%RC_SUBR%%
+
+name=sapserver
+rcvar=`set_rcvar`
+
+command=%%PREFIX%%/bin/${name}
+required_files=%%PREFIX%%/etc/sap.cfg
+
+load_rc_config ${name}
+run_rc_command "$1"
