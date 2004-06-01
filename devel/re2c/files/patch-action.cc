303e8d6c478667d8e95484e797561eacab54d9e2
ijliao <ijliao@freebsd.org>
2004-06-01 09:41:35 +0000

    upgrade to 0.9.3

---
+++ devel/re2c/files/patch-action.cc
@@ -0,0 +1,10 @@
+--- actions.cc.orig	Tue Jun  1 17:31:44 2004
++++ actions.cc	Tue Jun  1 17:32:05 2004
+@@ -1,6 +1,7 @@
+ /* $Id$ */
+ #include <time.h>
+ #include <string.h>
++#include <ctype.h>
+ #include <iostream>
+ #include <iomanip>
+ 
