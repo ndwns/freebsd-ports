16817e87e3f8085041676465bf4f06948618dbc8
pav <pav@freebsd.org>
2004-11-03 00:53:20 +0000

    - Update to 1.8
    PR:		ports/73376
    Submitted by:	Maxim Tuliuk <mt@primats.org.ua> (maintainer)

---
+++ palm/plucker/files/patch-PyPlucker::helper::prc.py
@@ -0,0 +1,7 @@
+--- PyPlucker/helper/prc.py.orig	Fri Nov  7 19:36:39 2003
++++ PyPlucker/helper/prc.py	Wed Dec 24 16:29:02 2003
+@@ -1,3 +1,4 @@
++# -*- coding: iso-8859-1 -*-
+ #
+ #  $Id$
+ #  Original RCSId: prc.py,v 1.6 1999/07/16 05:32:01 rob Exp 
--- palm/plucker/files/patch-parser::PyPlucker::Parser.py
+++
@@ -1,8 +0,0 @@
---- parser/PyPlucker/Parser.py.orig	Wed Dec 24 16:05:12 2003
-+++ parser/PyPlucker/Parser.py	Wed Dec 24 16:05:21 2003
-@@ -1,4 +1,5 @@
- #!/usr/bin/env python
-+# -*- coding: iso-8859-1 -*-
- 
- """
- Parser.py   $Id$
