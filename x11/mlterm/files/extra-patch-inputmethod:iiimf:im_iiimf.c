73b8589df642d8b1fc8fdf6b964cf9355662b9aa
nobutaka <nobutaka@freebsd.org>
2004-11-14 15:48:52 +0000

    - Update to 2.9.0. - Use OPTIONS. - Add three knobs(WITH_UIM, WITH_M17NLIB, WITH_IIIMF).
    Note: IIIMF support is currently broken.

---
+++ x11/mlterm/files/extra-patch-inputmethod:iiimf:im_iiimf.c
@@ -0,0 +1,10 @@
+--- inputmethod/iiimf/im_iiimf.c.orig	Sun Nov 14 21:01:46 2004
++++ inputmethod/iiimf/im_iiimf.c	Sun Nov 14 21:02:13 2004
+@@ -30,7 +30,6 @@
+  *	$Id$
+  */
+ 
+-#define  HAVE_STDINT_H 1	/* FIXME */
+ #include  <iiimcf.h>
+ 
+ #include  <string.h>		/* strncmp */
