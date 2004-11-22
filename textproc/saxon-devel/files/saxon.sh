752a27ede7f940a0c62177d61bac2a6a97896a8d
hq <hq@freebsd.org>
2004-11-22 16:49:34 +0000

    - Use MASTER_SITE_SOURCEFORGE - Track the recent changes to java/javavmwrapper - Various cosmetic changes

--- textproc/saxon-devel/files/saxon.sh
+++ textproc/saxon-devel/files/saxon.sh
@@ -17,4 +17,4 @@ do
 	SAXON_CLASSPATH="${SAXON_CLASSPATH}:${jarfile}"
 done
 
-JAVAVM="%%JAVAVM%%" "%%LOCALBASE%%/bin/javavm" -classpath "${SAXON_CLASSPATH}" "${LAUNCHER_CLASS}" "$@"
+JAVA_VERSION="1.4+" "%%LOCALBASE%%/bin/java" -classpath "${SAXON_CLASSPATH}" "${LAUNCHER_CLASS}" "$@"
