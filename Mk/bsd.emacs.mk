fb65ba6c25fb64afcbcc53e3ba2707771de4377e
sem <sem@freebsd.org>
2005-06-04 09:56:18 +0000

    * Update to 21.5-b21 "corn" * Unbreak on non-i386 platforms * Update EMACS_VER on editors/xemacs-devel-mule * Fix EMACS_VER on editors/xemacs-devel
    PR:		ports/81866
    Submitted by:	Andrey Slusar <anrays@gmail.com> (maintainer)

--- Mk/bsd.emacs.mk
+++ Mk/bsd.emacs.mk
@@ -166,7 +166,7 @@ PLIST?=                 ${PKGDIR}/pkg-plist.${EMACS_PORT_NAME}
 # XEmacs-21 development version
 .elif (${EMACS_PORT_NAME} == "xemacs-devel")
 EMACS_NAME=		xemacs
-EMACS_VER=		21.5-b17
+EMACS_VER=		21.5-b20
 EMACS_MAJOR_VER=	21
 EMACS_LIBDIR?=		lib/${EMACS_NAME}
 EMACS_LIBDIR_WITH_VER?=	lib/${EMACS_NAME}-${EMACS_VER}
@@ -183,7 +183,7 @@ PLIST?=                 ${PKGDIR}/pkg-plist.${EMACS_PORT_NAME}
 # XEmacs-21 development version with Mule
 .elif (${EMACS_PORT_NAME} == "xemacs-devel-mule")
 EMACS_NAME=		xemacs
-EMACS_VER=		21.5-b20
+EMACS_VER=		21.5-b21
 EMACS_MAJOR_VER=	21
 EMACS_LIBDIR?=		lib/${EMACS_NAME}
 EMACS_LIBDIR_WITH_VER?=	lib/${EMACS_NAME}-${EMACS_VER}
