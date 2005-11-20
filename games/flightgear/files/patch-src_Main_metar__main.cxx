a1f889bccf847ba7a6d1e0913e1e59d4ce5378f7
jylefort <jylefort@freebsd.org>
2005-11-20 22:16:58 +0000

    - Update to 0.9.9 - Move to ${X11BASE}

---
+++ games/flightgear/files/patch-src_Main_metar__main.cxx
@@ -0,0 +1,10 @@
+--- src/Main/metar_main.cxx.orig	Sun Nov 20 19:38:38 2005
++++ src/Main/metar_main.cxx	Sun Nov 20 19:39:05 2005
+@@ -20,6 +20,7 @@
+ //
+ // $Id$
+ 
++#include <time.h>
+ #include <iomanip>
+ #include <sstream>
+ #include <string.h>
