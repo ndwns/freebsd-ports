330242c3056de915de4fcbf30727fc2b168c99dd
marcus <marcus@freebsd.org>
2006-04-30 00:46:41 +0000

    Presenting GNOME 2.14.1 for FreeBSD!  Checkout http://www.gnome.org/start/2.14/ for the official release notes, and a list of all the gooides in this new release.  In particular, GNOME 2.14 focused on performance, and they did not miss the mark.  There's some new eye candy, but most of the big things are waiting until GNOME 2.16.  On the FreeBSD side, we tried to clean up all the crashers we could.  In particular, we really improved GNOME's 64-bit support.
    The good news is that this release does not bring any big shared library
    version bumps, so you can almost do a simple portupgrade to get to 2.14.
    There are a few minor gotchas that will be documented in UPDATING shortly.
    
    The FreeBSD GNOME Team would like th thank the following users for their
    patches, feedback, and sometimes incessant complaing about crashes (you
    know who you are).
    
    Yasuda Keisuke <kysd@po.harenet.ne.jp>
    Pascal Hofstee <caelian@gmail.com>
    rmgls@wanadoo.fr
    tmclaugh
    Yuri Pankov <yuri.pankov@gmail.com>
    sajd on #freebsd-gnome
    ade
    ankon on #FreeBSD-Gnome
    mux
    Pascal Hofstee <caelian@gmail.com>
    QuiRK on #freebsd-gnome
    Vladimir Timofeev <vovkasm@gmail.com>

--- x11/gnome-applets/files/patch-gweather_weather-iwin.c
+++
@@ -1,9 +0,0 @@
---- gweather/weather-iwin.c.orig	Tue Dec 21 18:42:38 2004
-+++ gweather/weather-iwin.c	Tue Dec 21 18:42:47 2004
-@@ -1,5 +1,6 @@
- /* $Id$ */
- 
-+#include <sys/types.h>
- #include <regex.h>
- #include <gnome.h>
- #include "weather.h"
