c810051cf252ba53596f5daaaa49032b1d743973
gabor <gabor@freebsd.org>
2007-08-04 11:37:23 +0000

    - Extract command variable definitions from bsd.port.mk, they are now stored in   bsd.commands.mk and can be easily reused within the infrastructure. - Revert old DESTDIR implementation. - Add a new, fully chrooted DESTDIR implementation as bsd.destdir.mk.
    Sponsored by:	Google Summer of Code 2007
    Approved by:	portmgr (pav)

--- Mk/bsd.emacs.mk
+++ Mk/bsd.emacs.mk
@@ -255,8 +255,8 @@ check-makevars::
 
 # find where emacsen is installed
 # look for it in PREEFIX first and fall back to LOCALBASE then
-.if exists(${TARGETDIR}/bin/${EMACS_NAME}-${EMACS_VER})
-EMACS_BASE?=			${TARGETDIR}
+.if exists(/bin/${EMACS_NAME}-${EMACS_VER})
+EMACS_BASE?=			${PREFIX}
 .else
 EMACS_BASE?=			${LOCALBASE}
 .endif
