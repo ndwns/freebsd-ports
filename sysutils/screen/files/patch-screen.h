c88396679229792720aed712cce33b5c2a9c9d06
edwin <edwin@freebsd.org>
2007-09-23 11:33:11 +0000

    sysutils/screen: install error if world compiled with NO_INFO
            The program /usr/bin/install-info doesn't exist in case
            the base system has been compiled with NO_INFO. This causes
            an error during installation of sysutils/screen because one
            of screen's make files rely on this program.
    
            I added handling of NO_INFO (and NO_MAN by the way). I suggest
            to let screen depend on print/texinfo if the user doesn't define
            NO_INFO and /usr/bin/install-info doesn't exist.
    
    PR:             ports/115048
    Submitted by:   Bj\xf6rn K\xf6nig <bkoenig@cs.tu-berlin.de>
    Approved by:    maintainer timeout

---
+++ sysutils/screen/files/patch-screen.h
@@ -0,0 +1,11 @@
+--- screen.h.orig	2003-08-22 14:28:43.000000000 +0200
++++ screen.h	2007-07-30 10:25:45.000000000 +0200
+@@ -22,6 +22,8 @@
+  * $Id$ FAU
+  */
+ 
++#include <string.h>
++
+ #include "os.h"
+ 
+ #if defined(__STDC__)
