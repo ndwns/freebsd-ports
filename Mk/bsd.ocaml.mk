90af007c81ef5a4717a7c35896476b406b325904
stas <stas@freebsd.org>
2008-05-19 07:05:35 +0000

    - Add a knob to generate pkg-plist for ocaml findlib   ports automatically. Now if USE_OCAMLFIND_PLIST is defined   all files under findlib target directories will be added   to pkg-plist and thus requires no entries in static pkg-plist.   This is important since a number of ocaml ports install its   package files conditionally depending on platform and ocaml   configurations and thus static entries are not possible.
    PR:		ports/122844 (based on)
    Submitted by:	Jaap Boender <jaapb@kerguelen.org>

--- Mk/bsd.ocaml.mk
+++ Mk/bsd.ocaml.mk
@@ -20,6 +20,8 @@
 # USE_OCAML_LDCONFIG	-	Set if your port installs shared libraries
 #				into ocaml site-lib dir. OCaml ld.conf file
 #				will be automatically processed.
+# USE_OCAMLFIND_PLIST	-	Add contents of findlib target directories
+#				automatically.
 # USE_OCAML_WASH	-	Set if your port wants to automatically
 #				purge shared Ocaml dirs on uninstall. It's
 #				useful when installing to non-standard PREFIX
@@ -122,6 +124,10 @@ pre-install-script:
 . if !target(ocaml-findlib)
 ocaml-findlib:
 .  for DIR in ${OCAML_PKGDIRS}
+.   if defined(USE_OCAMLFIND_PLIST)
+	@${FIND} ${PREFIX}/${OCAML_SITELIBDIR}/${DIR}/ -type f -print | ${SED} -e \
+		's,^${PREFIX}/,,' >> ${TMPPLIST}
+.   endif
 	@${ECHO_CMD} "@unexec rmdir %D/${OCAML_SITELIBDIR}/${DIR} 2>/dev/null || true" >> ${TMPPLIST}
 	@${ECHO_CMD} "@unexec ${OCAMLFIND} remove ${DIR} 2>/dev/null" \
 		>> ${TMPPLIST}
