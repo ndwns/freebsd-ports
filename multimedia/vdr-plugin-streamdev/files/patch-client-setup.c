da2d2afb58ab36480ffb0565c59da1e701d1132c
nox <nox@freebsd.org>
2011-03-26 19:20:47 +0000

    http://www.linuxtv.org/vdrwiki/index.php/Streamdev-plugin
    This PlugIn is a VDR implementation of the VTP (Video Transfer Protocol)
    Version 0.0.3 (see file PROTOCOL) and a basic HTTP Streaming Protocol.
    
    It consists of a server and a client part, but both parts are compiled together
    with the PlugIn source, but appear as separate PlugIns to VDR.
    
    The client part acts as a full Input Device, so it can be used in conjunction
    with a DXR3-Card, XINE, SoftDevice or others to act as a working VDR
    installation without any DVB-Hardware including EPG-Handling.
    
    The server part acts as a Receiver-Device and works transparently in the
    background within your running VDR. It can serve multiple clients and it can
    distribute multiple input streams (i.e. from multiple DVB-cards) to multiple
    clients using the native VTP protocol (for VDR-clients), or using the HTTP
    protocol supporting clients such as XINE, MPlayer and so on. With XMMS or
    WinAMP, you can also listen to radio channels over a HTTP connection.
    
    WWW: http://streamdev.vdr-developer.org/

---
+++ multimedia/vdr-plugin-streamdev/files/patch-client-setup.c
@@ -0,0 +1,10 @@
+--- client/setup.c.orig
++++ client/setup.c
+@@ -2,6 +2,7 @@
+  *  $Id$
+  */
+  
++#include <stdint.h>
+ #include <vdr/menuitems.h>
+ 
+ #include "client/setup.h"
---
+++ multimedia/vdr-plugin-streamdev/files/patch-server-setup.c
@@ -0,0 +1,10 @@
+--- server/setup.c.orig
++++ server/setup.c
+@@ -2,6 +2,7 @@
+  *  $Id$
+  */
+  
++#include <stdint.h>
+ #include <vdr/menuitems.h>
+ 
+ #include "server/setup.h"
