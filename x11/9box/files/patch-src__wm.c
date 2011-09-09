827f53e8e89004fde7fda9326c7bdfb089e35f3c
vanilla <vanilla@freebsd.org>
2011-09-09 01:14:55 +0000

    Fix build with clang.

---
+++ x11/9box/files/patch-src__wm.c
@@ -0,0 +1,11 @@
+--- src/wm.c.orig	2011-09-08 14:25:35.000000000 +0800
++++ src/wm.c	2011-09-08 14:25:46.000000000 +0800
+@@ -19,6 +19,8 @@
+ 
+ /* $Id$ */
+ 
++#include <stdio.h>
++#include <stdlib.h>
+ #include <X11/Xlib.h>
+ #include <X11/Xutil.h>
+ 
