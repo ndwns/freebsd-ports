c6c0aeb9416d37c32591ec94913f84e1482f7947
swills <swills@freebsd.org>
2011-12-29 23:24:35 +0000

    - Fix build on 10-CURRENT - Turn over to gnome team since it's used by Gnome ports
    PR:		ports/163635
    Submitted by:	swills (myself)
    Approved by:	melifaro (maintainer)

---
+++ net/opal3/files/patch-spandsp_if.cpp
@@ -0,0 +1,10 @@
+--- plugins/fax/fax_spandsp/spandsp_util/spandsp_if.cpp.orig	2011-12-26 21:04:48.000000000 +0000
++++ plugins/fax/fax_spandsp/spandsp_util/spandsp_if.cpp	2011-12-26 21:05:21.000000000 +0000
+@@ -23,6 +23,7 @@
+  * $Id$
+  */
+ 
++#define __STDC_LIMIT_MACROS
+ #include "spandsp_if.h"
+ 
+ #include <iostream>
