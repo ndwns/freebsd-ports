76f941b678d2e1e975d37c5c4822b0800bd5f5d6
fluffy <fluffy@freebsd.org>
2012-06-26 10:10:39 +0000

    - Update LLVM/Clang suite to 3.1 release
    PR:		168921
    Submitted by:	myself
    Approved by:	brooks (via pav@, with timeout)

--- lang/clang/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
+++ lang/clang/files/patch-tools_clang_include_clang_Analysis_Analyses_FormatString.h
@@ -3,14 +3,14 @@ $FreeBSD$
 
 --- tools/clang/include/clang/Analysis/Analyses/FormatString.h.orig
 +++ tools/clang/include/clang/Analysis/Analyses/FormatString.h
-@@ -139,6 +139,10 @@
-     SArg,
-     
+@@ -143,6 +143,10 @@
+ 
      // ** Printf-specific **
-+
+ 
 +    // FreeBSD specific specifiers
 +    bArg,
 +    DArg,
-   
++
      // Objective-C specific specifiers.
      ObjCObjArg,  // '@'
+     ObjCBeg = ObjCObjArg, ObjCEnd = ObjCObjArg,
